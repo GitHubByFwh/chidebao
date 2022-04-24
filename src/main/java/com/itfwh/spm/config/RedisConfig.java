8bf3298a949581f48a8e52ec229affe38b0e24b9
Git_fwh <1963113211@qq.com>
2022-04-24 23:23:28 +0800

    Redis环境的配置

---
+++ src/main/java/com/itfwh/spm/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.itfwh.spm.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author:Fwh
+ * @date 2022/4/24 23:17
+ * @ClassName RedisConfig
+ * @Description redis序列化器的管理
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的key的序列化器 ： JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
